b678f05c49b787ced44eb79b2ff1bae5b91bfdb1
patchipulusujayesh <patchipulusujayesh@gmail.com>
2026-01-03 22:19:00 +0530

    Q18.py

---
+++ Q18.py
@@ -0,0 +1,14 @@
+secret_number = 7
+
+while True:
+    guess = int(input("Guess the number: "))
+
+    if guess > secret_number:
+        print("Too high")
+    elif guess < secret_number:
+        print("Too low")
+    else:
+        print("Correct! You guessed the number.")
+        break
+
+
